6e8a5f03719f954544683820ec4c73bec8a4168e
unknown <attaway.code@gmail.com>
2024-03-18 18:51:31 -0700

    adds seeder along with files to seed the graphql db

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
-const { User, Product, Category, Order } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
-const stripe = require('stripe')('');
+const { User, Product, Category, Order } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+const stripe = require("stripe")("");
 // add in strip key^
 const resolvers = {
   Query: {
@@ -16,20 +16,27 @@ const resolvers = {
 
       if (name) {
         params.name = {
-          $regex: name
+          $regex: name,
         };
       }
 
-      return await Product.find(params).populate('category');
+      return await Product.find(params).populate("category");
+    },
+    productsTest: async (parent, { category, name }) => {
+      try {
+        return Product.find({});
+      } catch {
+        throw AuthenticationError;
+      }
     },
     product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
+      return await Product.findById(_id).populate("category");
     },
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
+          path: "orders.products",
+          populate: "category",
         });
 
         user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
@@ -42,8 +49,8 @@ const resolvers = {
     order: async (parent, { _id }, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
+          path: "orders.products",
+          populate: "category",
         });
 
         return user.orders.id(_id);
@@ -61,11 +68,11 @@ const resolvers = {
       for (const product of args.products) {
         line_items.push({
           price_data: {
-            currency: 'usd',
+            currency: "usd",
             product_data: {
               name: product.name,
               description: product.description,
-              images: [`${url}/images/${product.image}`]
+              images: [`${url}/images/${product.image}`],
             },
             unit_amount: product.price * 100,
           },
@@ -74,9 +81,9 @@ const resolvers = {
       }
 
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
+        payment_method_types: ["card"],
         line_items,
-        mode: 'payment',
+        mode: "payment",
         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${url}/`,
       });
@@ -95,7 +102,9 @@ const resolvers = {
       if (context.user) {
         const order = new Order({ products });
 
-        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+        await User.findByIdAndUpdate(context.user._id, {
+          $push: { orders: order },
+        });
 
         return order;
       }
@@ -104,7 +113,9 @@ const resolvers = {
     },
     updateUser: async (parent, args, context) => {
       if (context.user) {
-        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+        return await User.findByIdAndUpdate(context.user._id, args, {
+          new: true,
+        });
       }
 
       throw AuthenticationError;
@@ -112,7 +123,11 @@ const resolvers = {
     updateProduct: async (parent, { _id, quantity }) => {
       const decrement = Math.abs(quantity) * -1;
 
-      return await Product.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
+      return await Product.findByIdAndUpdate(
+        _id,
+        { $inc: { quantity: decrement } },
+        { new: true }
+      );
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
@@ -130,8 +145,8 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
-    }
-  }
+    },
+  },
 };
 
 module.exports = resolvers;
---
+++ server/seeders/seed.js
@@ -0,0 +1,50 @@
+const db = require("../config/connection");
+const { User /* Thought */, Product } = require("../models");
+const userSeeds = require("./userSeeds.json");
+// const thoughtSeeds = require("./thoughtSeeds.json");
+const cleanDB = require("../config/cleanDB");
+
+db.once("open", async () => {
+  // we need
+  await cleanDB("User", "users");
+  try {
+    await User.create(userSeeds);
+    console.log("Collection cleaned successfully.");
+  } catch (err) {
+    console.error("Error cleaning collection:", err);
+  }
+
+  await cleanDB("Product", "products");
+  try {
+    const request = await fetch(
+      "https://mock.shop/api?query={products(first:%2020){edges%20{node%20{id%20title%20description%20tags%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}"
+    );
+
+    if (!request.ok) {
+      throw new Error("something went wrong!");
+    }
+
+    const { data } = await request.json();
+    const products = data.products.edges.map((edge) => {
+      const product = edge.node;
+      product.price = product.variants.edges[0].node.price;
+      return product;
+    });
+
+    const loadedProductData = products.map((loadedProduct) => ({
+      id: loadedProduct.id,
+      name: loadedProduct.title,
+      description: loadedProduct.description, // You may need to adjust this depending on the actual response structure
+      image: loadedProduct.featuredImage.url || "",
+      category: loadedProduct.tags,
+      price: loadedProduct.price,
+    }));
+
+    console.log(loadedProductData);
+    Product.insertMany(loadedProductData);
+  } catch (err) {
+    console.error(err);
+  }
+  console.log("all done!");
+  process.exit(0);
+});
